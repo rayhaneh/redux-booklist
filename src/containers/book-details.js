f446cb372b1397ffe532c79b726cf88396e3f0fc
Rayhaneh <r.b.assady@gmail.com>
2017-10-29 10:53:42 -0400

    Added two containers for book list and book details.

---
+++ src/containers/book-details.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class BookDetails extends Component {
+  render() {
+    return(
+      <div>
+        Book Details
+      </div>
+    )
+  }
+}
