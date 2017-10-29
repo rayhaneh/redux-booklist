d0bb105419f9d138e0fefc385907f14455c68c06
Rayhaneh <r.b.assady@gmail.com>
2017-10-29 11:52:09 -0400

    Added a few comments for clarity

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,6 @@
 export default function selectBook(activeBook) {
+  // selectBook is an action creator. 
+  // It needs to creat and action, an object with a type property.
   return {
     type: 'BOOK_SELECTED',
     payload: activeBook
--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+// Redux serves to construct application's state
+// React provides the views to display that state
+// The two libraries are inherintly discounected and it's only through
+// the react-redux that we clearly get a connection between the two.
 import { connect } from 'react-redux';
 import selectBook from '../actions/index';
 import { bindActionCreators } from 'redux';
@@ -30,14 +34,20 @@ class BookList extends Component {
 
 
 function mapStateToProps(state) {
+  // Whatever is returned here will show up as props inside booklist
   return {
     books: state.books
   }
 }
 
-
+// Anything returned from this function will end up as props on bookList container
 function mapStatetoDispatch(dispatch) {
+  // Whenever selectBook is called, the results should be passed to all our reducers
+  // That's what bindActionCreators does, it takes the results of the action and runs
+  // it through dispatch
   return bindActionCreators({ selectBook: selectBook}, dispatch);
 }
 
+// Promote booklist from a component to a container - it needs to know about the
+// dispatch method, selectBook. Make it avaiable as a prop.
 export default connect(mapStateToProps, mapStatetoDispatch)(BookList)
